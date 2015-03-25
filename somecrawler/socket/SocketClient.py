711cfb281f1ac8c23c0e855ee4c833eb66102476
Resul42 <Resul-g_93@hotmail.com>
2015-03-25 15:35:57 +0100

    host and port added in config

--- somecrawler/socket/SocketClient.py
+++ somecrawler/socket/SocketClient.py
@@ -1,11 +1,11 @@
 __author__ = 'resul'
 import socket
+from somecrawler.config import Config
 
 u = "'0868049', 'passwordd'"
-HOST = ''    # The remote host
-PORT = 50100              # The same port as used by the server
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s.connect((Config.SOCKET_HOST, Config.SOCKET_PORT))
 s.sendall(u)
 data = s.recv(1024)
 s.close()
